9f5b83c4d19f8ce3abc73616ba08ce6231637f18
tylercho <36307133+TylerChoinsky@users.noreply.github.com>
2023-06-22 06:49:45 -0400

    Update background.js
    Added a functionality explanation comment, removed a redundant string replacement function execution.

--- background.js
+++ background.js
@@ -1,9 +1,16 @@
+// In a nutshell, it does this:
+// ...
+// If the icon is clicked:
+//    check which website you are on
+//    if the website you are on is valid (9anime or mangadex):
+//        let the extension create and open the necessary link to find reddit discussions
+
 chrome.action.onClicked.addListener(tab => {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
         let url = tabs[0].url;
         let site = url.split("/")[2];
         if (site == "9anime.to") {
-            let anime = (((url.split("/")[4]).split(".")[0]).replace(/-/g, " ")).replace(/ /g, "+");
+            let anime = (((url.split("/")[4]).split(".")[0]).replace(/-/g, "+"));
             let episode = (url.split("/")[5]).split("-")[1];
             let link = "https://www.reddit.com/r/anime/search?q=" + anime + "+episode+" + episode + "+discussion&restrict_sr=on&include_over_18=on&sort=relevance&t=all";
             chrome.tabs.create({ url: link });
