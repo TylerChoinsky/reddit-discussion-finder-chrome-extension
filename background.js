230402ef521fbcffa8e31eea222a170a7e2a8616
tylercho <36307133+TylerChoinsky@users.noreply.github.com>
2023-10-29 00:45:42 -0400

    Update background.js
    9anime.to rebranded to aniwave.to . This commit reflects that change.

--- background.js
+++ background.js
@@ -9,7 +9,7 @@ chrome.action.onClicked.addListener(tab => {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
         let url = tabs[0].url;
         let site = url.split("/")[2];
-        if (site == "9anime.to") {
+        if (site == "aniwave.to") {
             let anime = (((url.split("/")[4]).split(".")[0]).replace(/-/g, "+"));
             let episode = (url.split("/")[5]).split("-")[1];
             let link = "https://www.reddit.com/r/anime/search?q=" + anime + "+episode+" + episode + "+discussion&restrict_sr=on&include_over_18=on&sort=relevance&t=all";
